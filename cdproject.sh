21c62b54462455e7afd16d0eb4da352e182b03ce
Andreas Stocker <andreas@stockers.org>
2020-01-13 23:54:32 +0200

    fix handling of local paths provided to idrisExec

--- cdproject.sh
+++ cdproject.sh
@@ -1,5 +1,5 @@
 x=`dirname $1`
-while [ "$x" != "/" ] ; do
+while [ "$x" != "/" ] && [ "$x" != "." ] ; do
     find "$x" -maxdepth 1 -name *.ipkg | egrep '.*' && cd "$x" && break
     x=`dirname "$x"`
 done
