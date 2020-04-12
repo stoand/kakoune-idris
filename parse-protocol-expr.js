fb4d761ce62fba0e8b4cb3faf32de6428278bf95
Andreas Stocker <andreas@stockers.org>
2020-04-12 19:55:26 +0300

    parse-protocol-expr: readPrefixedLines

--- parse-protocol-expr.js
+++ parse-protocol-expr.js
@@ -1,6 +1,20 @@
-function parseProtocolExpr() {
-    console.log('todo');
+let endLine = 'Alas the file is done, aborting';
+let sizePrefixLength = 6;
 
+// converts lines in the format of 0000<length>(:data ..)0000<length>(:more ..) to ['(:data ..', '(:more ..']
+function readPrefixedLines(str) {
+    let lines = [];
+    while(str.indexOf(endLine) !== 0) {
+        let size = parseInt(str.slice(0, sizePrefixLength), 16);
+        lines.push(str.slice(sizePrefixLength, size + sizePrefixLength - 1));
+        str = str.slice(size + sizePrefixLength);
+    }
+
+    return lines;
+}
+
+function parseProtocolExpr(str) {
+    console.log(readPrefixedLines(str));
     return [];
 }
 
