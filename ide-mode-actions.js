d232778b26ac29090e1ccada64c41ea942cd71bc
stoand <stoand@protonmail.com>
2020-10-01 17:41:54 +0300

    actions: remove need for ipkg and kakoune pwd paths

--- ide-mode-actions.js
+++ ide-mode-actions.js
@@ -15,12 +15,10 @@ function extract(fileContents, regex, defaultValue) {
     return defaultValue;
 }
 
-function idrisExec(file, ipkg, root, additionalCommand, next) {
-    let cdProjectCmd = 'cd ' + root + ' >> /dev/null';
-
+function idrisExec(file, additionalCommand, next) {
     // idris2 --ide-mode always returns status 1 (error) because the last line sent was empty
     try {
-        execSync(cdProjectCmd + '; [[ -d ' + root + ' ]] && cd ' + root + '; idris2 --find-ipkg --ide-mode',
+        execSync(`cd $(dirname ${file}); idris2 --find-ipkg --ide-mode`,
         	{ input: `((:load-file "${file}") 1)\n` + additionalCommand + '\n', encoding: 'utf8', shell: '/bin/bash' });
     } catch (res) {
 
@@ -31,10 +29,20 @@ function idrisExec(file, ipkg, root, additionalCommand, next) {
         // Prefer to use the warning message over the error message
         if (warn) {
             let filePath = warn[1][0];
+        
             let [line, column] = warn[1][1];
             let msg = warn[1][3];
             let escapedMsg = msg.replace(/\\"/g, '""'); // kakoune uses "" instead of \" to escape "
-            return `e "${filePath}" ${line} ${column}; info "${escapedMsg}"`;
+
+            // only jump to the current file
+            // this is a hack limited by the fact we dont know the ipkg directory
+            // if we knew the ipkg directory we could jump to other files
+            if (file.indexOf(filePath) !== -1) {
+                return `e "${file}" ${line} ${column}; info "${escapedMsg}"`;
+            } else {
+                return `info "${escapedMsg}"`;
+            }
+            
         } else if (err) {
             let msg = err[1][1];
             return `info "${msg}"`;
@@ -49,61 +57,59 @@ function lastRetVal(exprs) {
 }
 
 exports.load = function(file, ipkg, root) {
-    return idrisExec(file, ipkg, root, '', () => {});
+    return idrisExec(file, '', () => {});
 }
 
-exports.interpret = function(file, ipkg, root, selection) {
-    return idrisExec(file, ipkg, root, `((:interpret "${selection}") 1)`, exprs => {
+exports.interpret = function(file, selection) {
+    return idrisExec(file, `((:interpret "${selection}") 1)`, exprs => {
         return `echo "${lastRetVal(exprs)}"`;
     });
 }
 
-exports.typeOf = function(file, ipkg, root, selection, line, column) {
-    return idrisExec(file, ipkg, root, `((:type-of "${selection}" ${line} ${column}) 1)`, exprs => {
+exports.typeOf = function(file, selection, line, column) {
+    return idrisExec(file, `((:type-of "${selection}" ${line} ${column}) 1)`, exprs => {
         return `info -title "idris-ide: type" "\n${lastRetVal(exprs)}"`;
     });
 }
 
-exports.caseSplit = function(file, ipkg, root, selection, line, column) {
-    return idrisExec(file, ipkg, root, `((:case-split ${line} "${selection}") 1)`, exprs => {
+exports.caseSplit = function(file, selection, line, column) {
+    return idrisExec(file, `((:case-split ${line} "${selection}") 1)`, exprs => {
         let generatedCode = lastRetVal(exprs);
         return `execute-keys -draft x c "${newLinesToRet(generatedCode)}<ret><esc>"; execute-keys ${line}g ${column - 1}l`;
     });
     
 }
 
-exports.addClause = function(file, ipkg, root, selection, line) {
-    return idrisExec(file, ipkg, root, `((:add-clause ${line} "${selection}") 1)`, exprs => {
+exports.addClause = function(file, selection, line) {
+    return idrisExec(file, `((:add-clause ${line} "${selection}") 1)`, exprs => {
         let generatedCode = lastRetVal(exprs);
         return `execute-keys -draft o "${newLinesToRet(generatedCode)}<esc>"; execute-keys jwwb`;
     });
 }
 
-exports.proofSearch = function(file, ipkg, root, selection, line) {
-    return idrisExec(file, ipkg, root, `((:proof-search ${line} "${selection}") 1)`, exprs => {
+exports.proofSearch = function(file, selection, line) {
+    return idrisExec(file, `((:proof-search ${line} "${selection}") 1)`, exprs => {
         let generatedCode = lastRetVal(exprs);
         return `execute-keys -draft c <backspace> "${newLinesToRet(generatedCode)}<esc>"`;
     });
 }
 
-exports.generateDef = function(file, ipkg, root, selection, line) {
-    return idrisExec(file, ipkg, root, `((:generate-def ${line} "${selection}") 1)`, exprs => {
+exports.generateDef = function(file, selection, line) {
+    return idrisExec(file, `((:generate-def ${line} "${selection}") 1)`, exprs => {
         let generatedCode = lastRetVal(exprs);
         return `execute-keys -draft o "${newLinesToRet(generatedCode)}<esc>"; execute-keys jwwb`;
     });
 }
 
-exports.makeLemma = function(file, ipkg, root, selection, line) {
-    return idrisExec(file, ipkg, root, `((:make-lemma ${line} "${selection}") 1)`, exprs => {
+exports.makeLemma = function(file, selection, line) {
+    return idrisExec(file, `((:make-lemma ${line} "${selection}") 1)`, exprs => {
         let [generatedCode, replace] = lastRetVal(exprs).split('\n');
         return `execute-keys c <backspace> "${replace}" <esc> <A-i> p O "${generatedCode}" <ret> <esc> k`;
     });
 }
 
-// Make Case - TOOD (not implemented in idris2 as of this time)
-
-exports.makeWith = function(file, ipkg, root, selection, line) {
-    return idrisExec(file, ipkg, root, `((:make-with ${line} "${selection}") 1)`, exprs => {
+exports.makeWith = function(file, selection, line) {
+    return idrisExec(file, `((:make-with ${line} "${selection}") 1)`, exprs => {
         let generatedCode = lastRetVal(exprs);
         return `execute-keys -draft o "${newLinesToRet(generatedCode)}<backspace><esc>"; execute-keys -with-maps -with-hooks j <A-l> h c`;
     });
--- ide-mode-test.js
+++ ide-mode-test.js
@@ -17,74 +17,74 @@ try{ fs.rmdirSync('build', { recursive: true }); } catch (e) {}
 
 
 // We need to preload the files to have deterministic messages later
-actions.load(testSrc, testIpkg, testRoot);
-actions.load(testInvalidSrc, testIpkg, testRoot);
+actions.load(testSrc);
+actions.load(testInvalidSrc);
 
 assert.equal(
-	actions.interpret(testNonExistingSrc, testIpkg, testRoot, '', 1, 1),
+	actions.interpret(testNonExistingSrc, '', 1, 1),
 	`info "Error loading file ${testNonExistingSrc}: File Not Found"`,
     'Attempt to load wrong file should display command failed');
 
 // Don't display additional error info for incorrect files
 // Expect the developer to be building the file in another shell tab
 assert.equal(
-	actions.interpret(testInvalidSrc, testIpkg, testRoot, '', 1, 1),
-	`e "src/TestInvalid.idr" 4 8; info "While processing right hand side of asdf. Can't find ` +
+	actions.interpret(testInvalidSrc, '', 1, 1),
+	`e "${__dirname}/tests/src/TestInvalid.idr" 4 8; info "While processing right hand side of asdf. Can't find ` +
     'an implementation for FromString Integer.\n\n' +
     'src/TestInvalid.idr:4:8--4:15\n   |\n 4 | asdf = ""wrong""\n   ' +
     '|        ^^^^^^^\n"',
     'Attempt to load invalid file should display command failed');
 
 assert.equal(
-	actions.interpret(testSrc, testIpkg, testRoot, '2+2', 1, 1),
+	actions.interpret(testSrc, '2+2', 1, 1),
 	`echo "4"`,
     'Interpret');
 
 // It appears the ide mode server does not handle escaping correctly yet
 // assert.equal(
-// 	actions.interpret(testSrc, testIpkg, testRoot, `"a"`, 1, 1),
+// 	actions.interpret(testSrc, `"a"`, 1, 1),
 // 	`echo "4"`,
 //     'Interpret correctly handles quotes');
 
 assert.equal(
-	actions.typeOf(testSrc, testIpkg, testRoot, 'caseSplitHere_rhs', 1, 1),
+	actions.typeOf(testSrc, 'caseSplitHere_rhs', 1, 1),
 	`info -title "idris-ide: type" "\n   splitHere : Bool\n` +
     '------------------------------\ncaseSplitHere_rhs : ' +
     `String"`,
     'Type of');
 
 assert.equal(
-	actions.caseSplit(testSrc, testIpkg, testRoot, 'splitHere', 4, 15),
+	actions.caseSplit(testSrc, 'splitHere', 4, 15),
 	`execute-keys -draft x c "caseSplitHere True = ?caseSplitHere_rhs_1<ret>` +
 	`caseSplitHere False = ?caseSplitHere_rhs_2<ret><esc>"; execute-keys 4g 14l`,
     'Clause split runs new line with clause');
 
 assert.equal(
-	actions.addClause(testSrc, testIpkg, testRoot, 'addClauseHere', 6, 1),
+	actions.addClause(testSrc, 'addClauseHere', 6, 1),
 	'execute-keys -draft o "addClauseHere xs = ?addClauseHere_rhs<esc>"; execute-keys jwwb',
     'Add clause');
 
 assert.equal(
-	actions.proofSearch(testSrc, testIpkg, testRoot, 'proofSearchHere_rhs', 9, 1),
+	actions.proofSearch(testSrc, 'proofSearchHere_rhs', 9, 1),
 	'execute-keys -draft c <backspace> "Refl<esc>"',
     'Proof search');
 
 assert.equal(
-	actions.generateDef(testSrc, testIpkg, testRoot, 'generateDefHere', 11, 1),
+	actions.generateDef(testSrc, 'generateDefHere', 11, 1),
 	'execute-keys -draft o "generateDefHere True = ' +
     'Refl<ret>generateDefHere False = Refl<esc>"; ' +
     'execute-keys jwwb',
     'Generate definition');
 
 assert.equal(
-	actions.makeLemma(testSrc, testIpkg, testRoot, 'makeLemmaHere_rhs', 14, 1),
+	actions.makeLemma(testSrc, 'makeLemmaHere_rhs', 14, 1),
 	'execute-keys c <backspace> "makeLemmaHere_rhs y x" <esc> ' +
     '<A-i> p O "makeLemmaHere_rhs : Int -> Int -> Int" <ret> ' +
     '<esc> k',
     'Make lemma');
 
 assert.equal(
-	actions.makeWith(testSrc, testIpkg, testRoot, 'x', 17, 1),
+	actions.makeWith(testSrc, 'x', 17, 1),
 	'execute-keys -draft o "makeWithHere x with (_)<ret>  makeWithHere x | ' +
     'with_pat = ?x_rhs<backspace><esc>"; execute-keys -with-maps -with-hooks j <A-l> ' +
     'h c',
